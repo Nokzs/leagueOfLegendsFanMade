43fcd472be10a899763293b3993ef9e7b6f5be88
alexis <alexischapusot@gmail.com>
2023-08-09 16:50:33 +0200

    mis en place du router V0.1

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
+import FrontPage from './Componenents/FrontPage'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FrontPage />,
+    
+  },
+  {
+    path:"/champions",
+    
+  },
+  {
+    path:'/build',
+  }
+]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </React.StrictMode>,
 )
