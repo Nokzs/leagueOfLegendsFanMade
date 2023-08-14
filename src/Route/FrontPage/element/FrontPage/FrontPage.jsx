7a55c3a85245ad4ef49270b343f2e65ee23ecb63
alexis <alexischapusot@gmail.com>
2023-08-14 16:01:28 +0200

    ajout

--- src/Route/FrontPage/element/FrontPage/FrontPage.jsx
+++ src/Route/FrontPage/element/FrontPage/FrontPage.jsx
@@ -1,21 +1,23 @@
 import { NavLink } from "react-router-dom"
 import "./FrontPage.css"
-import { useState } from "react";
+import { useRef } from "react"
 
 
 export const FrontPage = ()=>{
-    
+
+   
     return(
+        <>
         <div className="background">
            <video autoPlay muted className="background-video">
           <source src="src/Route/FrontPage/asset/a.mp4" type="video/mp4" />
          </video>
             <nav className="navLink">
                 <li className="listlink">
-                    <ul>
-                        <div className="buttonLink">
-                            <NavLink className={({isActive,isPending})=>isPending ? "pending":isActive? "active":""}to={"/"}>acceuil</NavLink>
-                        </div>
+                    <ul >
+                        <div  className="buttonLink" >
+                            <NavLink  className={({isActive,isPending})=>isPending ? "pending":isActive? "active":""}to={"/"}>acceuil</NavLink>
+                        </div>  
                     </ul>
                     <ul>
                         <div className="buttonLink">
@@ -31,7 +33,8 @@ export const FrontPage = ()=>{
             <div className="trailerContainer">
             <iframe className="trailer" width="560" height="315" src="https://www.youtube.com/embed/cXZqfuJ9Zps" title="YouTube video player"  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
             </div>
-            
         </div>
+        
+        </>
     )
 }
