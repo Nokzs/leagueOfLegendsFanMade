92ead4df8cf71d258ce2be610b6b452432575969
alexis <alexischapusot@gmail.com>
2023-08-15 20:45:37 +0200

    reparation

--- src/Route/FrontPage/component/HomePage/HomePage.jsx
+++ src/Route/FrontPage/component/HomePage/HomePage.jsx
@@ -1,18 +1,13 @@
 
 import "./HomePage.css"
 
-import { Roles } from "./Roles/Roles"
-import { NavBar } from "./NavBar/NavBar"
-import { CSSTransition } from 'react-transition-group';
-import { useState } from "react"
-
 export const HomePage =()=>{
-    const [isStarted,setIsStarted] = useState(false);
     
-    return (
-        <>
-        <NavBar/>
-        <Roles/>
-        </>
+    return(
+     
+        <div className="Home">
+           
+        </div>
+       
     )
 }
