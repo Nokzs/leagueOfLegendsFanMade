b79970be53068629b77b7dc60e248504552c43e7
Vopexx <neil.hadibi1@etu.univ-lorraine.fr>
2023-08-18 00:35:08 +0200

    amélioration roles

--- src/Route/FrontPage/component/HomePage/Roles/Roles.jsx
+++ src/Route/FrontPage/component/HomePage/Roles/Roles.jsx
@@ -5,57 +5,67 @@ import { useState } from "react";
 
 
 export const Roles = () => {
-    const [circlex, setX] = useState(0);
+    const [circlex, setCircleX] = useState(0);
     const refCircle = useRef(null);
     const refButton = useRef(null);
+
+    const [opacityHover, setOp] = useState(false);
+
+    const opacityReset = (e) => {
+        e.currentTarget.style = "";
+    }
+    
     const moveCircle = (e) => {
-        let c = refCircle.current.getBoundingClientRect()
+        
         let pos = e.currentTarget.getBoundingClientRect()
         let ppos = e.currentTarget.parentNode.getBoundingClientRect()
-       
-        
-        setX((pos.x+(pos.width/2)-ppos.x)*100/ppos.width  - (c.width/2*100/ppos.width))
+        setCircleX((pos.x+(pos.width/2)-ppos.x)*100/ppos.width  - (refCircle.current.getBoundingClientRect().width/2*100/ppos.width))
+        setOp(true);
     };
-    useEffect(()=>{
-        let c = refCircle.current.getBoundingClientRect()
+
+    useEffect(() => {
         let pos = refButton.current.getBoundingClientRect()
         let ppos = refButton.current.parentNode.getBoundingClientRect()
-        setX((pos.x+(pos.width/2)-ppos.x)*100/ppos.width  - (c.width/2*100/ppos.width))
+        setCircleX((pos.x+(pos.width/2)-ppos.x)*100/ppos.width  - (refCircle.current.getBoundingClientRect().width/2*100/ppos.width))
     },[])
     
     return(
         <>
             <section className="roles">
-                <div className="rolesBox"></div>
+                <div className="rolesBox">
+                {/*utiliser flex*/}
+                    <div style={{background:"blue", width:30+'%', height:90+'%'}}></div> 
+                    <div style={{background:"red", width:40+'%', height:90+'%'}}></div>
+                </div>
 
                 <div className="listBox">
-                    <div  style={{left:`${circlex}%`}} className="movingCircle" ref={refCircle}>
-                            <Svg name="Circle"/>
+                    <div onAnimationEnd={() => {setOp(false)}} style={{left:`${circlex}%`}} className={"movingCircle"+(opacityHover ? " anim":"")} ref={refCircle}>
+                        <Svg name="Circle"/>
                     </div>
                     <div className="catContainer">
-                        <button ref={refButton} onClick={moveCircle}>
+                        <button ref={refButton} onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Assassins" />
                             <span>Assassins</span>
                         </button>
-                        <button onClick={moveCircle}>
+                        <button onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Combattants"/>
-                            <span>Assassins</span>
+                            <span>Combattants</span>
                         </button>
-                        <button onClick={moveCircle}>
+                        <button onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Mages"/>
-                            <span>Assassins</span>
+                            <span>Mages</span>
                         </button>
-                        <button onClick={moveCircle}>
+                        <button onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Tireurs"/>
-                            <span>Assassins</span>
+                            <span>Tireurs</span>
                         </button>
-                        <button onClick={moveCircle}>
+                        <button onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Supports"/>
-                            <span>Assassins</span>
+                            <span>Supports</span>
                         </button>
-                        <button onClick={moveCircle}>
+                        <button onMouseEnter={moveCircle} onMouseLeave={opacityReset}>
                             <Svg name="Tanks"/>
-                            <span>Assassins</span>
+                            <span>Tanks</span>
                         </button>
                     </div>
                     
